96ebf9407a9f4e932d01777aaa478b2631133ee3
Gustavo <gustavorossich@gmail.com>
2022-08-18 13:54:52 +0200

    Starting with Deck and Card classes

---
+++ deckbuild.py
@@ -0,0 +1,84 @@
+import random
+
+class Card:
+  """
+      Gaming card class.
+      Initialises the class with all the possibles combinations of suit
+      and rank, and assigns a value of 0 to each card, needs to be inherited 
+      for the corresponding game
+  """
+  def __init__(self,suit, rank):
+        self.suit = suit
+        self.rank = rank
+        self.value = 0
+
+  def ranksuit(self):
+      """Method for printing the rank and suit"""
+      return f'{self.rank} of {self.suit}'
+  
+
+class Bjcard(Card):
+  """
+    Gaming card class. The values are set for the BlackJack game
+    Inherits from class Card
+  """
+  
+  VALUES = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
+          'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
+  
+  def __init__(self, suit, rank):
+    """ Initializes the values corresponding to the Black Jack game """
+    super().__init__(suit, rank)
+    self.value = VALUES[rank]
+    
+
+
+class Deck:
+  """
+      Creates a deck, with methods to shuffle cards
+      and deal one card to the player
+      
+      Normal Deck
+      
+      Methods:
+      --------
+      shuffle_cards : returns none
+      deal_one      : returns the dealt card (Card) and removes it from the Deck
+  """
+  def __init__(self):
+    """For each suit and rank, create a card object"""
+    
+    # Standard SUIT and RANKS 
+    SUITS = ('Hearts','Diamonds','Spades','Clubs')
+    RANKS = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine',
+            'Ten', 'Jack', 'Queen', 'King', 'Ace')
+            
+    self.cards = []
+    for suit in SUITS:
+        for rank in RANKS:
+            self.cards.append(Card(suit,rank))
+
+  def shuffle_cards(self):
+      """Imports random for shuffling the deck"""
+      random.shuffle(self.cards)
+
+  def deal_one(self):
+      """Deals one to the player and removes it from the deck"""
+      return self.cards.pop()
+
+
+class Dtruco:
+  
+  def __init__(self):
+    
+    SUITS = ('Espada','Basto','Copas','Oro')
+    RANKS = ('Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco', 'Seis', 'Siete',
+              'Diez', 'Once', 'Doce')
+              
+    # Add all the cards in the Deck          
+    self.cards = []
+    for suit in SUITS:
+        for rank in RANKS:
+            self.cards.append(Card(suit,rank))
+    
+#TODO replace deal one method. Cards doesn't leave the deck
---
+++ truco.py
@@ -0,0 +1,3 @@
+#TODO Get the base deck class from the other project -> import random
+#TODO Make envido points
+#TODO Cards values
