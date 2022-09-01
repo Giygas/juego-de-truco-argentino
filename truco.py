6d30656723de66c43b1fd6338670e1d3451b1482
Gustavo <gustavorossich@gmail.com>
2022-09-01 22:18:18 +0200

    Added what to do in truco game

--- truco.py
+++ truco.py
@@ -3,3 +3,17 @@
 # Truco points counted per round
 # This class creates the teams (team 1 and team 2)
 # This class should manage who's mano
+# The player that wins plays first the second hand
+# And there the round continue
+
+# Maybe create a list of players and his turns, then continue from the player that won
+# Team mano keeps counting if all the players have a draw
+
+# team 1 player 1 plays
+# ask for input
+# # show possibilities, cantar truco, envido o flor
+# # last round check for 4 in the other team
+# team 2 player 1 plays
+# team 1 player 2 plays
+# team 2 player 2 plays
+
